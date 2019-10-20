08a396a2ed22f9b209055609fa96a3fcca85d7e4
tharindu97 <tharindukavishna@gmail.com>
2019-10-20 20:03:27 +0100

    step2

--- app/src/main/java/com/example/home/MainActivity.kt
+++ app/src/main/java/com/example/home/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +18,14 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity","Button was clicked")
 
             Toast.makeText(this,"click here",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+
+            Toast.makeText(this,"login here",Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
