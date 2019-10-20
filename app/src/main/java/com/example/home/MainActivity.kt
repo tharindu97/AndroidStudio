13d478d55b202f35d3aa81d59a2c2ed662368e6b
tharindu97 <tharindukavishna@gmail.com>
2019-10-20 13:27:43 +0100

    Tstep1

---
+++ app/src/main/java/com/example/home/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnShoeToast.setOnClickListener {
+            Log.i("MainActivity","Button was clicked")
+
+            Toast.makeText(this,"click here",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
